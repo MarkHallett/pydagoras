480f5c7f6efffd9a97dce4d385eaf9229d20a2c7
MarkHallett <git@markhallett.co.uk>
2025-08-17 16:53:34 +0100

    simple use

--- tests/eg_use_pydagoras.py
+++ tests/eg_use_pydagoras.py
@@ -3,51 +3,30 @@
 
 from pydagoras.dag_dot import DAG_dot, calc
 
-class EgDag(DAG_dot):
-    def __init__(self):
-        self.title = 'Eg DAG'
-        super().__init__(self.title)
-
-        # define nodes
-        self.o = self.makeNode(label='Out', calc=self.calc_output, usedby = [], nodetype='out')
-        self.aa = self.makeNode(label='calc_A', calc=self.calcRateA, usedby=[self.o], nodetype='internal', tooltip='x2')
-        self.a = self.makeNode(label='A', calc=None, usedby=[self.aa], nodetype='in', display_name='In', tooltip='in')
-
-    @calc
-    def calcRateA(self, node):
-        return self.a.get_value() * 2 
-
-    @calc
-    def calc_output(self, node):
-        return self.aa.get_value()
-
-
 def run():
 
-    print('\n** Create the DAG')
-    my_dag = EgDag()
+    print('#######################################')
 
-    print('\n** print DAG initial output')
-    print (my_dag.G.to_string())
+    @calc
+    def tripple(node=None):
+        return node.get_value() * 3
 
-    print('\n** set input to 4')
-    EgDag().set_input('A',4 ) 
+    dag = DAG_dot(label='Eg DAG')
+    n2 = dag.makeNode('x3', calc=tripple, tooltip='multiply')
+    n1 = dag.makeNode('In', calc=None, usedby=[n2], nodetype='in')
 
-    print('\n** set input to 6')
-    EgDag().set_input('A',6 ) 
+    print('Updates --------------')
+    dag.set_input('In', 10)
 
-    print('\n** print DAG final output')
-    print (my_dag.G.to_string())
+    print('Outputs --------------')
+    dag.ppInputs()
+    dag.ppOutput()
+    dag.pp()
 
-    EgDag().ppInputs()
-    return
-    EgDag().ppOutput()
-    
-    print('\n** XXXXXXXX print DAG final output')
-    print (my_dag.G.to_string())
-    print (my_dag.pp())
+    print('Graph representation:')
+    print(dag.G.to_string())  # Print the graph representation
+
 
 if __name__ == '__main__':
     run()
     print('Done.')
-
